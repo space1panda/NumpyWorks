bcdf1b33e077b1aefd204d1391876a46644eca6f
spacepanda <iegormykhailov@gmail.com>
2019-07-10 06:44:54 +0200

    copying np_projects to separated repo

---
+++ datasources/chardatasource.py
@@ -0,0 +1,72 @@
+import numpy as np
+import re
+from utils.charutils_np import chunks
+
+class CharDatasource:
+
+    def __init__(self, path, seq_len, *args):
+
+        """Initialize vocabularies for char rnn.
+        In our case we want to train only characters and \n (end of word) token
+         Cleaning data with regex: \n character left to indicate end of token(word)
+        to train  ending word, cleaning all symbols and spaces
+        """
+
+        data = open(path, 'r').read().lower()
+        data = re.sub('[^0-9a-zA-Z\n]+', '', data)
+        chars = list(set(data))
+
+        self._seq_len = seq_len
+        self._char2ix = {None:0}
+        self._char2ix.update({ch: i+1 for i, ch in enumerate(sorted(chars))})
+        self._ix2char = {0:None}
+        self._ix2char.update({i+1: ch for i, ch in enumerate(sorted(chars))})
+        self._vocab_size = len(self._char2ix)
+
+        """Initialize vocabulary with char appearance statistics. 
+        It can be useful to determine unfrequently used characters to omit them in training. 
+        In this experiment however we are not excluding characters due to small dataset.  
+        """
+
+        self._char_stats = {char: 0 for char in sorted(chars)}
+
+        # Initialize x, y sets as empty lists
+
+        self._tokens = []
+        self._targets = []
+
+        """Call generator to encode input and output sequences. 
+        Output is a one-step forward sequence in the same datasource - 
+        We want our model to learn predicting next character
+        """
+
+        source = chunks(data, seq_len)
+
+        while True:
+            try:
+                s = next(source)
+                for ch in s:
+                    self._char_stats[ch] += 1
+                self._tokens.append([self._char2ix[None]] + [self._char2ix[ch] for ch in s])
+                self._targets.append([self._char2ix[ch] for ch in s] + [self._char2ix['\n']])
+
+            except StopIteration:
+                break
+
+        """Vectorize with numpy and quickly one-hot the input set.
+        Final shapes = tokens - (num_of_sequences, sequence length, vocab_size)
+        targets - (num_of_sequences, sequence length, char2idx[character])
+        We are not creating one-hot for outputs because we want to count loss 
+        between only most-probable character index and target (see sampling in inference)
+        """
+
+        self._tokens = np.array(self._tokens[:-1], dtype=np.int32)
+        self._targets = np.array(self._targets[:-1])
+        x_values = np.max(self._tokens) + 1
+        self._tokens = np.eye(x_values)[self._tokens]
+
+    def __getitem__(self, idx):
+        return self._tokens[idx], self._targets[idx]
+
+    def __len__(self):
+        return len(self._tokens)
